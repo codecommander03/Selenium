918ba5fb8069ed9e3f384536f5b7abc0caaec71a
Aditya <codecommander03@gmail.com>
2023-12-08 00:22:19 +0530

    minor changes

--- main.py
+++ main.py
@@ -26,20 +26,20 @@ isNextDisabled = False
 
 while not isNextDisabled:
     try:
-        element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.s-main-slot.s-result-list.s-search-results.sg-row")))
+        element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@data-component-type="s-search-result"]')))
 
         elem_list = browser.find_element(By.CSS_SELECTOR, "div.s-main-slot.s-result-list.s-search-results.sg-row")
 
         items = elem_list.find_elements(By.XPATH, '//div[@data-component-type="s-search-result"]')
 
         for item in items:
-            title = item.find_element(By.TAG_NAME, "h2").text
+            title = item.find_element(By.TAG_NAME, 'h2').text
             price = "No Price Found"
-            img = "No Imafe Found"
-            link = item.find_element(By.CLASS_NAME, 'a-link-normal').get_attribute("href")
+            img = "No Image Found"
+            link = item.find_element(By.CLASS_NAME, 'a-link-normal').get_attribute('href')
 
             try:
-                price = item.find_element(By.CSS_SELECTOR, '.a-price').text.replace("\n", " ")
+                price = item.find_element(By.CSS_SELECTOR, '.a-price').text.replace("\n", ".")
             except:
                 pass
 
@@ -51,7 +51,7 @@ while not isNextDisabled:
             print("Title: " + title)
             print("Price: " + price)
             print("Image: " + img)
-            print("Link: "+ link + "\n")
+            print("Link: " + link + "\n")
 
             write_json({
                 "title": title,
@@ -61,14 +61,14 @@ while not isNextDisabled:
             })
 
         next_btn = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 's-pagination-next')))
-        next_class = next_btn.get_attribute("class")
-        
+
+        next_class = next_btn.get_attribute('class')
+
         if "disabled" in next_class:
             isNextDisabled = True
-            break
         else:
             browser.find_element(By.CLASS_NAME, 's-pagination-next').click()
-        
+
     except Exception as e:
         print(e, "Main Error")
         isNextDisabled = True
